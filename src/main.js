6e7e11ee97f6b03908b5ca02d257e5e63f267daf
İlhan Aydınlı <13945364+ilhanaydinli@users.noreply.github.com>
2021-08-14 16:10:19 +0300

    Added pwa.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify'
 import i18n from './plugins/i18n'
 import router from './router'
 import store from './store'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
+  transpileDependencies: ['vuetify'],
 
   pluginOptions: {
     i18n: {
